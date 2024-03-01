7ead8d1ca147723c60993e89b43160afa053272a
Raphael <rt.raphael.oliveira@gmail.com>
2024-03-01 13:40:01 -0300

    Feat - Ajuste no envio do email

--- app.js
+++ app.js
@@ -32,49 +32,28 @@ async function run() {
 
 		const { body, validationResult } = require("express-validator");
 
-		app.post(
-			"/email",
-			[
-				body("name").notEmpty(),
-				body("email").notEmpty().isEmail(),
-				body("password").notEmpty().isLength({ min: 6 })
-			],
-			async (req, res) => {
-				const errors = validationResult(req);
-				if (!errors.isEmpty()) {
+		app.post("/email", async (req, res) => {
+			const { _id, date, text, completed, user } = req.body;
+
+			const msg = {
+				to: user,
+				from: "rt.raphael.oliveira@gmail.com", // Apenas para teste
+				subject: "Alteração na tarefa",
+				html: `Oi, sua tarefa: "${req.body.text}" foi alterada!</strong>`
+			};
+
+			sgMail
+				.send(msg)
+				.then(() => {
+					return res.send(`Email Eviado`);
+				})
+				.catch((error) => {
+					console.error(error);
 					return res
 						.status(400)
-						.send(
-							`Não foi possível validar o email e/ou a senha ${errors}\n`
-						);
-				}
-
-				const { name, email, password } = req.body;
-
-				const msg = {
-					to: "raphaelbrasil7@gmail.com", // Change to your recipient
-					from: "rt.raphael.oliveira@gmail.com", // Change to your verified sender
-					subject: "Sending with SendGrid is Fun",
-					text: `and easy to do anywhere, even with Node.js`,
-					html: `<strong>and easy to do anywhere, even with Node.js. Here is the message :${req.body.email}</strong>`
-				};
-
-				sgMail
-					.send(msg)
-					.then(() => {
-						console.log("Email sent");
-						return res.send(`Email Eviado`);
-					})
-					.catch((error) => {
-						console.error(error);
-						return res
-							.status(400)
-							.send(
-								`Não foi possível enviar o email: ${errors}\n`
-							);
-					});
-			}
-		);
+						.send(`Não foi possível enviar o email: ${error}\n`);
+				});
+		});
 
 		app.post(
 			"/profiles",
